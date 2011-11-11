6b760790b985a407c304aea251a8aec14f582128
Alex <aperlscript@gmail.com>
2011-11-11 14:47:15 -0800

    The "Tile" class no longer tracks direction

--- Tile.java
+++ Tile.java
@@ -9,7 +9,6 @@ public class Tile implements Serializable {
 	public Tile(int row, int col) {
 		this.row = row;
 		this.col = col;
-		dir = 0;
 		letter = Empty;
 		letterBonus = 1;
 		wordBonus = 1;
@@ -18,9 +17,7 @@ public class Tile implements Serializable {
 	}
 	
 	// "row" and "col" are 0-index
-	// "dir" is direction if this tile is the start of a word: 0:none, 1:up,
-	// 2:right, 3:down, 4:left
-	public int row, col, dir;
+	public int row, col;
 	
 	// the letter that is currently on the tile
 	public char letter;
