57cd5f97e68f251a89889d5b42bbf4990903e79f
Alex <aperlscript@gmail.com>
2011-11-11 14:46:41 -0800

    the "Word" class now implements "Comparable"

--- Word.java
+++ Word.java
@@ -1,7 +1,24 @@
 package com.alex.scrabblesolver;
 
-public class Word {
+import java.util.ArrayList;
+
+public class Word implements Comparable<Word> {
 	public String word;
 	public int score;
 	public Tile first;
+	public String orientation;
+	public ArrayList<String> bonuses;
+	
+	// Comparable
+	public int compareTo(Word o) {
+		if (this.score < o.score) {
+			return -1;
+		}
+		else if (this.score > o.score) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
 }
