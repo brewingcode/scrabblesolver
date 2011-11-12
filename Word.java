36e6423cfd2513a4129e08bf689de1d2df5b0a4a
Alex <aperlscript@gmail.com>
2011-11-11 16:53:09 -0800

    minor tweaks to Word class

--- Word.java
+++ Word.java
@@ -8,12 +8,16 @@ public class Word implements Comparable<Word> {
 	public String where;
 	public ArrayList<String> bonuses;
 	
+	public Word() {
+		bonuses = new ArrayList<String>();
+	}
+	
 	// Comparable
 	public int compareTo(Word o) {
-		if (this.score < o.score) {
+		if (this.score > o.score) {
 			return -1;
 		}
-		else if (this.score > o.score) {
+		else if (this.score < o.score) {
 			return 1;
 		}
 		else {
@@ -22,18 +26,14 @@ public class Word implements Comparable<Word> {
 	}
 	
 	public String toString() {
-		String me = String.format("%03d %s %s", score, where, word.toUpperCase());
+		String me = String.format("%3d  %s  %s", score, where, word.toUpperCase());
 		if (bonuses.size() > 0) {
-			me += bonuses.toString();
+			me += "  " + bonuses.toString();
 		}
 		return me;
 	}
 	
 	public void addBonuses(int wordBonus, int letterBonus) {
-		if (bonuses == null) {
-			bonuses = new ArrayList<String>();
-		}
-		
 		if (wordBonus == 2) bonuses.add("DW");
 		if (wordBonus == 3) bonuses.add("TW");
 		if (letterBonus == 2) bonuses.add("DL");
